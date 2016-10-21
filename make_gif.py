23d74479671927e4ae8d0067f041de9c1dcd1d2c
Andrew Fulton <andrewfulton9@gmail.com>
2016-10-21 12:17:47 -0600

    minor edits

--- make_gif.py
+++ make_gif.py
@@ -3,8 +3,8 @@ from PIL import Image
 import os
 
 def make_gif(folder = 'images', name = 'CO_pop_gif.GIF', save_to = 'plots'):
-    file_names = sorted((fn for fn in os.listdir('images') if fn.endswith('.png')))
-    images = [('images/' + fn) for fn in file_names]
+    file_names = sorted((fn for fn in os.listdir(folder) if fn.endswith('.png')))
+    images = [('folder' + fn) for fn in file_names]
     frames = [imageio.imread(im) for im in images]
     save_path = save_to + '/' + name
     imageio.mimsave(save_path, frames, 'GIF', **{'duration':.1})
@@ -25,7 +25,7 @@ def get_to_folder(dirs):
     save_to = None
     while save_to not in dirs:
         print 'directories to choose from: ', dirs
-        save_to = raw_input('where would you like to save GIF?')
+        save_to = raw_input('where would you like to save GIF?: ')
         check_dir(save_to, dirs)
     return save_to
 
@@ -33,11 +33,11 @@ def where_to():
     dirs = next(os.walk('.'))[1]
     folder = get_from_folder(dirs)
     save_to = get_to_folder(dirs)
-    name = raw_input('what would you like to name your GIF?')
+    name = raw_input('what would you like to name your GIF?: ')
     return folder, save_to, name
 
 if __name__ == '__main__':
-    yn = raw_input('Custom save? type y for yes, n for no')
+    yn = raw_input('Custom save? type y for yes, n for no: ')
     if yn == 'y':
         f, st, n = where_to()
         make_gif(folder = f, name = n, save_to = st)
